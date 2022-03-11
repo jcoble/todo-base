29eb81ad742e8c643f4f96d7030847c68aae097d
jcoble <coble.jesse@gmail.com>
2022-03-11 13:28:34 -0500

    Update store.ts

--- src/store.ts
+++ src/store.ts
@@ -1,3 +1,6 @@
+import { proxy } from "valtio";
+import  { todoStore } from "../Store"
+
 // Standard interface and functions
 export interface Todo {
   id: number;
@@ -5,19 +8,22 @@ export interface Todo {
   done: boolean;
 }
 
-export const updateTodo = (todos: Todo[], id: number, text: string): Todo[] =>
-  todos.map((todo) => ({
-    ...todo,
-    text: todo.id === id ? text : todo.text,
-  }));
-
+interface todoStore {
+  id: number;
+  todos: Todo[];
+  newTodo: "";
+  addTodo: () => void;
+  removeTodo: (id: number) => void;
+  toggleTodo: (id: number) => boolean;
+  updateTodo: (id: number, text: string) => void;
+}
 export const toggleTodo = (todos: Todo[], id: number): Todo[] =>
   todos.map((todo) => ({
     ...todo,
-    done: todo.id === id ? !todo.done : todo.done,
+    done: todo.id === id ? !todo.done : todo.done
   }));
 
-export const removeTodo = (todos: Todo[], id: number): Todo[] =>
+export const removeTodo = (todos: Todo[], id: number): Tcodo[] =>
   todos.filter((todo) => todo.id !== id);
 
 export const addTodo = (todos: Todo[], text: string): Todo[] => [
@@ -25,6 +31,34 @@ export const addTodo = (todos: Todo[], text: string): Todo[] => [
   {
     id: Math.max(0, Math.max(...todos.map(({ id }) => id))) + 1,
     text,
-    done: false,
-  },
+    done: false
+  }
 ];
+export const updateTodo = (todos: Todo[], id: number, text: string): Todo[] =>
+  todos.map((todo) => ({
+    ...todo,
+    text: todo.id === id ? text : todo.text
+  }));
+
+const todoState = proxy<todoStore>({
+  todos = [],
+  newTodo = "",
+  addTodo() {
+    todoState.todos = addTodo(todoState.todos, todoState.newTodo);
+    todoState.newTodo = "";
+  },
+  removeTodo() {
+    todoState.todos = removeTodo(todoState.todos, todoState.id);
+  },
+  toggleTodo() {
+    todoState.todos = toggleTodo(todoState.todos, todoState.id);
+  },
+  updateTodo() {
+    todoState.todos = updateTodo(
+      todoState.todos,
+      todoState.id,
+      todoState.newTodo
+    );
+  }
+});
+export default todoState;
