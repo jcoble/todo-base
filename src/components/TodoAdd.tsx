5b1edf13c127b60cca26ab74c13dcf977825d88b
jcoble <coble.jesse@gmail.com>
2022-03-11 13:29:16 -0500

    Update TodoAdd.tsx

--- src/components/TodoAdd.tsx
+++ src/components/TodoAdd.tsx
@@ -1,11 +1,17 @@
 import * as React from "react";
 import { Button, Input, Grid } from "@chakra-ui/react";
+import { useProxy, useSnapshot} from "valtio";
+import { todoStore} "../store";
 
 function TodoAdd() {
+  const snapShot = useProxy(todo);
+
+  
+
   return (
     <Grid pt={2} templateColumns="5fr 1fr" columnGap="3">
-      <Input placeholder="New todo" />
-      <Button>Add Todo</Button>
+      <Input placeholder="New todo"  value={snapShot.newTodo} onChange={(ev) => {todoStore.newTodo.text = e.target.value}}/>
+      <Button onClick={() => todoStore.addTodo()>Add Todo</Button>
     </Grid>
   );
 }
